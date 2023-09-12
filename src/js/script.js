08e46477d8f3288da09d405af3fd473eb747cb9a
Sachin <sachin333777@outlook.com>
2023-09-12 20:20:46 +0530

    position the share and logic

--- src/js/script.js
+++ src/js/script.js
@@ -1,14 +1,29 @@
-const STRANGE_STRING = "hello world"
-function findSize () {
-  const upDownLen = STRANGE_STRING.toUpperCase().toLowerCase().length;
-  const originalLen = STRANGE_STRING.length;
-  
-  if (upDownLen > originalLen) {
-    return upDownLen;
-  }
-  else {
-    return originalLen;
-  }
-}
+class App {
+    constructor() {
+        this.icon = document.querySelector('[data-id="icon-share"]');
+        this.toggleShareMenu = document.querySelector('.hidden');
+    };
 
-console.log(findSize());
+    init() {
+        this.icon.addEventListener("click", (e) => {
+            e.preventDefault();
+            this.hide();
+
+        });
+    };
+
+    hide() {
+        if (this.toggleShareMenu.style.display === 'none') {
+            this.toggleShareMenu.style.display = 'flex';
+        } else {
+            this.toggleShareMenu.style.display = 'none';
+        }
+    };
+
+
+};
+
+const dom = new App;
+
+dom.init();
+
