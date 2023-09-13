e0321b4a2cb38934c65d95078b4773e9037eb94e
Sachin <Sachin333777@outlook.com>
2023-09-13 09:18:36 +0530

    eslint config

--- src/js/script.js
+++ src/js/script.js
@@ -1,29 +1,25 @@
 class App {
-    constructor() {
-        this.icon = document.querySelector('[data-id="icon-share"]');
-        this.toggleShareMenu = document.querySelector('.hidden');
-    };
+  constructor() {
+    this.icon = document.querySelector('[data-id="icon-share"]');
+    this.toggleShareMenu = document.querySelector('.hidden');
+  }
 
-    init() {
-        this.icon.addEventListener("click", (e) => {
-            e.preventDefault();
-            this.hide();
+  init() {
+    this.icon.addEventListener('click', (e) => {
+      e.preventDefault();
+      this.hide();
+    });
+  }
 
-        });
-    };
+  hide() {
+    if (this.toggleShareMenu.style.display === 'none') {
+      this.toggleShareMenu.style.display = 'flex';
+    } else {
+      this.toggleShareMenu.style.display = 'none';
+    }
+  }
+}
 
-    hide() {
-        if (this.toggleShareMenu.style.display === 'none') {
-            this.toggleShareMenu.style.display = 'flex';
-        } else {
-            this.toggleShareMenu.style.display = 'none';
-        }
-    };
-
-
-};
-
-const dom = new App;
+const dom = new App();
 
 dom.init();
-
